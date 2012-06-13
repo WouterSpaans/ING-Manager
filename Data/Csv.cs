00e648a81f847046a736ea429445ce23ce8517f2
Wouter Spaans <WouterSpaans@Hotmail.com>
2012-06-13 16:15:49 +0200

    Initial Project

---
+++ Data/Csv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ING_Manager.Data
+{
+    using System.IO;
+
+    public class Csv
+    {
+        public static void Parse(DirectoryInfo csvDirectory)
+        {
+            foreach (FileInfo csvFile in csvDirectory.EnumerateFiles("*.csv"))
+            {
+                Console.WriteLine("  Parsing file: {0}", csvFile.Name);
+            }
+        }
+    }
+}
---
+++ TransactionParser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ING_Manager.TransactionParser
+{
+    using System.IO;
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ING Transaction Parser...");
+          
+            string csvFolder = args.Any() ? args[0] : Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Data.Csv.Parse(new DirectoryInfo(csvFolder));
+            
+            Console.ReadLine();
+        }
+    }
+}
